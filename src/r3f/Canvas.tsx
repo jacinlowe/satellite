1eaa28991906e0997b7b32438245395df041298e
Mike <mikematics22800@gmail.com>
2024-12-29 17:00:44 -0500

    commit

--- src/r3f/Canvas.tsx
+++ src/r3f/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense,useRef,useState } from 'react';
+import { Suspense,useRef } from 'react';
 import { Canvas, useFrame,  } from '@react-three/fiber';
 import { Stats, OrbitControls } from '@react-three/drei'
 import * as THREE from 'three';
--- src/r3f/OrbitLines.tsx
+++ src/r3f/OrbitLines.tsx
@@ -3,32 +3,32 @@ import { useRef,useFrame } from 'react';
 import { Line,LineBasicMaterial,CircleGeometry, Circle } from '@react-three/drei';
 
 
-// function OrbitLine(){
-//     const geometry = new CircleGeometry(5,32);
-//     const material = new LineBasicMaterial({color:0xffff00});
+ function OrbitLine(){
+     const geometry = new CircleGeometry(5,32);
+     const material = new LineBasicMaterial({color:0xffff00});
 
-//     return <Line geometry={geometry} material={material}/>
+     return <Line geometry={geometry} material={material}/>
 
-// }
+ }
 
 export default OrbitLine;
 
-// function OrbitLine() {
-//     const ref = useRef(null);
-//     // useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01));
+ function OrbitLine() {
+     const ref = useRef(null);
+      useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01));
  
-//     return (
-//        <mesh
-//           visible
-//           position={[0, 0, 0]}
-//           rotation={[0, 0, 0]}
-//           castShadow
-//           ref={ref}>
-//              <ringBufferGeometry args={[1, 4, 32]} />
-//              <meshBasicMaterial attach="material" color="hotpink" />
-//        </mesh>
-//     );
-//  }
+     return (
+        <mesh
+           visible
+           position={[0, 0, 0]}
+           rotation={[0, 0, 0]}
+           castShadow
+           ref={ref}>
+              <ringBufferGeometry args={[1, 4, 32]} />
+              <meshBasicMaterial attach="material" color="hotpink" />
+        </mesh>
+     );
+  }
 
 function OrbitLine() {
     return <Circle/> 
